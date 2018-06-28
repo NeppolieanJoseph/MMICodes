46b3886730d66316bd71dfb210b60506ec2fae7d
neppoliean <neppoliean.joseph@aspiresys.com>
2018-06-28 15:05:46 +0530

    Update deploywithoutpython.py

--- deploywithoutpython.py
+++ deploywithoutpython.py
@@ -1,6 +1,10 @@
 from distutils.core import setup
+from py2exe.build_exe import py2exe
+from PyQt5 import *
+from PyQt4 import *
 import py2exe
-setup(console=['deploy_win_standalone.py'])
+#setup(console=['tkinter.py'])
+setup(windows=['tkinter.py'], options={"py2exe":{"includes":["sip"]}})
 
 #To execute this script run the following command in the CMD
 #python deploywithoutpython.py   py2exe --bundle-files 1 --dll-excludes w9xpopen.exe --dist-dir deployment_build
